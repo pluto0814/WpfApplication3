4b23c4f5cca3a59e23270684401404c31e9c27f7
Stefan Buschmann <tux2k@live.de>
2018-05-18 14:18:56 +0200

    Projektdateien hinzufügen.

---
+++ WpfApplication3/WPFTest.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
